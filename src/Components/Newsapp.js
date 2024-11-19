6bcfacb2e7cfd7b6490a3f191786f3bf74a7d465
RahulVerma799 <160318545+RahulVerma799@users.noreply.github.com>
2024-11-19 15:30:49 +0530

    jsonn

--- src/Components/Newsapp.js
+++ src/Components/Newsapp.js
@@ -12,7 +12,7 @@ const Newsapp = () => {
         const response = await fetch(`${PROXY_URL}https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`);
         const jsonData = await response.json();
         console.log(jsonData.articles);
-        let dt = jsonData.articles.slice(0,10)
+        let dt = jsonData.articles
         setNewsData(dt)
         
     }
